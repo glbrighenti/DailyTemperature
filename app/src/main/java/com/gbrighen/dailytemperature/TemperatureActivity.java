c91a43f42b2390ae01afa25658f2f4580eb1a002
Guilherme Brighenti <glbrighenti@gmail.com>
2016-02-10 22:26:25 -0200

    adding temperature sensor support

--- app/src/main/java/com/gbrighen/dailytemperature/TemperatureActivity.java
+++ app/src/main/java/com/gbrighen/dailytemperature/TemperatureActivity.java
@@ -1,5 +1,10 @@
 package com.gbrighen.dailytemperature;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,19 +15,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class TemperatureActivity extends AppCompatActivity {
+public class TemperatureActivity extends AppCompatActivity implements SensorEventListener {
 
-    public native float convertToCelsius(float t);
+    private native float convertToCelsius(float t);
+
+    private SensorManager sensorManager;
+    private Sensor tempSensor;
 
     static {
         System.loadLibrary("conversion_module");
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -34,24 +46,29 @@ public class TemperatureActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_temperature, menu);
-        return true;
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float temperature = sensorEvent.values[0];
+        getSupportActionBar().setTitle("temp="+temperature);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, tempSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
 }
